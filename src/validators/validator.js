0c1fbd578aadc47ea833fe7a106240d5431ed708
Hariprasadcm08 <hariprasadcm08@gmail.com>
2022-12-05 03:53:54 +0530

    final- push

---
+++ src/validators/validator.js
@@ -0,0 +1,18 @@
+const stringChecking=function(data){
+    if(typeof data!=='string'){
+        return false;
+    }else if(typeof data=="string"&&data.length==0){
+        return false
+    }else {
+        return true
+    }
+}
+
+
+const isvalidEmail=/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
+const isvalidMobile=/^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/
+const isValidName=/^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/
+const isValidlogolink=/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/
+const isValidAbbrv=/^[.a-zA-Z\s,-]+$/
+
+module.exports={stringChecking,isvalidEmail,isvalidMobile,isValidName,isValidlogolink,isValidAbbrv}
